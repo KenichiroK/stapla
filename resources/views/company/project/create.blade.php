9cdb20425ca7e20e84174f1b6e7f015b2e9fd967
Keisuke Tsuji <kei.iwmmd@gmail.com>
2019-07-03 18:41:46 +0900

    html,css修正

--- resources/views/company/project/create.blade.php
+++ resources/views/company/project/create.blade.php
@@ -27,92 +27,92 @@
 
 @section('content')
 <div class="project__create__main__container">
-    <form aciton="{{ url('/project/create') }}" method='POST'  class="main__container__wrapper">
+    <form aciton="{{ url('/project/create') }}" method='POST'>
     {{ csrf_field() }}
-
-        <div class="project__create__main__container__wrapper">
-            <h1 class="main__container__wrapper__title">プロジェクト作成</h1>
-            <div class="project-create__container">
-                <ul class="project-create__container__list">
-                    <li class="project-create__container__list__item margin--none">
-                        <div class="project-create__container__list__item__name">プロジェクト名 :</div>
-                        <div class="control">
-                            <input name="name" class="project-create__container__list__item__input under-border input" type="text" placeholder="プロジェクト名">
+    <div class="project__create__main__container__wrapper">
+        <div class="top-container">
+            <h1 class="top-container__title">プロジェクト作成</h1>
+        </div>
+        <div class="project-create__container">
+            <ul class="project-create__container__list">
+                <li class="project-create__container__list__item margin--none">
+                    <div class="project-create__container__list__item__name">プロジェクト名 :</div>
+                    <div class="control">
+                        <input name="name" class="project-create__container__list__item__input under-border input" type="text" placeholder="プロジェクト名">
+                    </div>
+                </li>
+                <li class="project-create__container__list__item">
+                    <div class="project-create__container__list__item__name">プロジェクト詳細 :</div>
+                    <div class="control">
+                        <textarea class="project-create__container__list__item__textarea textarea has-fixed-size" name="detail" placeholder=""></textarea>
+                    </div>
+                </li>
+                <li class="project-create__container__list__item">
+                    <div class="project-create__container__list__item__name">担当者 :</div>
+                    <div class="select id-normal">
+                        <select name="company_user_id" class="select-box" id="company-staff-name-list">
+                            <option></option>
+                            @foreach( $company_infos as $company_info )
+                            <option value={{ $company_info->id }}>{{ $company_info->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </li>
+                <li class="project-create__container__list__item">
+                    <div class="project-create__container__list__item__name">パートナー :</div>
+                    <div class="select id-normal">
+                        <select name="partner_id" class="select-box" id="partner-name-list">
+                            <option value=""></option>
+                            @foreach( $partner_infos as $partner_info )
+                            <option value={{ $partner_info->id }}>{{ $partner_info->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>      
+                </li>
+                <li class="project-create__container__list__item">
+                    <div class="project-create__container__list__item__name">プロジェクト期間 :</div>
+                    <div class="calendars">
+                        <div class="calendars__wrapper">
+                            <div class="calendars__wrapper__title">開始日</div>
+                            <input name="started_at" type="text">
                         </div>
-                    </li>
-                    <li class="project-create__container__list__item">
-                        <div class="project-create__container__list__item__name">プロジェクト詳細 :</div>
-                        <div class="control">
-                            <textarea class="project-create__container__list__item__textarea textarea has-fixed-size" name="detail" placeholder=""></textarea>
+                        <div class="calendars__wrapper">
+                            <div class="calendars__wrapper__title">終了日</div>
+                            <input name="ended_at" type="text">
                         </div>
-                    </li>
-                    <li class="project-create__container__list__item">
-                        <div class="project-create__container__list__item__name">担当者 :</div>
-                        <div class="select id-normal">
-                            <select name="company_user_id" class="select-box" id="company-staff-name-list">
-                                <option></option>
-                                @foreach( $company_infos as $company_info )
-                                <option value={{ $company_info->id }}>{{ $company_info->name }}</option>
-                                @endforeach
-                            </select>
+                    </div>
+                </li>
+                <li class="project-create__container__list__item">
+                    <div class="project-create__container__list__item__name">予算 :</div>
+                    <div class="budget-container control">
+                        <input name="budget" class="input is-normal" type="text" placeholder=""><span class="budget-container__yen">円</span>
+                    </div>      
+                </li>
+                <li class="project-create__container__list__item">
+                    <div class="project-create__container__list__item__name">資料 :</div>
+                    <div class="project-create__container__list__item__wrapper">
+                        <div class="project-create__container__list__item__wrapper__description">Upload</div>
+                        <div class="file is-boxed project-create__container__list__item__wrapper__filearea">
+                            <label class="file-label project-create__container__list__item__wrapper__filearea__label">
+                                <input class="file-input project-create__container__list__item__wrapper__filearea__label" type="file">
+                                <span class="file-cta project-create__container__list__item__wrapper__filearea__label__file-cta">
+                                <span class="file-icon">
+                                    <i class="fas fa-upload"></i>
+                                </span>
+                                <span class="file-label">
+                                    Choose a file…
+                                </span>
+                                </span>
+                            </label>
                         </div>
-                    </li>
-                    <li class="project-create__container__list__item">
-                        <div class="project-create__container__list__item__name">パートナー :</div>
-                        <div class="select id-normal">
-                            <select name="partner_id" class="select-box" id="partner-name-list">
-                                <option value=""></option>
-                                @foreach( $partner_infos as $partner_info )
-                                <option value={{ $partner_info->id }}>{{ $partner_info->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>      
-                    </li>
-                    <li class="project-create__container__list__item">
-                        <div class="project-create__container__list__item__name">プロジェクト期間 :</div>
-                        <div class="calendars">
-                            <div class="calendars__wrapper">
-                                <div class="calendars__wrapper__title">開始日</div>
-                                <input name="started_at" type="text">
-                            </div>
-                            <div class="calendars__wrapper">
-                                <div class="calendars__wrapper__title">終了日</div>
-                                <input name="ended_at" type="text">
-                            </div>
-                        </div>
-                    </li>
-                    <li class="project-create__container__list__item">
-                        <div class="project-create__container__list__item__name">予算 :</div>
-                        <div class="budget-container control">
-                            <input name="budget" class="input is-normal" type="text" placeholder=""><span class="budget-container__yen">円</span>
-                        </div>      
-                    </li>
-                    <li class="project-create__container__list__item">
-                        <div class="project-create__container__list__item__name">資料 :</div>
-                        <div class="project-create__container__list__item__wrapper">
-                            <div class="project-create__container__list__item__wrapper__description">Upload</div>
-                            <div class="file is-boxed project-create__container__list__item__wrapper__filearea">
-                                <label class="file-label project-create__container__list__item__wrapper__filearea__label">
-                                    <input class="file-input project-create__container__list__item__wrapper__filearea__label" type="file">
-                                    <span class="file-cta project-create__container__list__item__wrapper__filearea__label__file-cta">
-                                    <span class="file-icon">
-                                        <i class="fas fa-upload"></i>
-                                    </span>
-                                    <span class="file-label">
-                                        Choose a file…
-                                    </span>
-                                    </span>
-                                </label>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-                <div class="button-wrapper">
-                    <button type="submit" class="button-wrapper__btn button">作成</button>
-                </div>
+                    </div>
+                </li>
+            </ul>
+            <div class="button-wrapper">
+                <button type="submit" class="button-wrapper__btn button">作成</button>
             </div>
         </div>
-
-</form>
+    </div>
+    </form>
 </div>
 @endsection
