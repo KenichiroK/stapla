e6bd5cd1920e1964cbdcb2980993c842fcfa6330
Keisuke Tsuji <kei.iwmmd@gmail.com>
2019-07-09 15:38:31 +0900

    バリデーションのエラーメッセージおよび、バリデーションにかかったあとに入力文字が残るようにするまで（selectだけ未対応）

--- resources/views/company/project/create.blade.php
+++ resources/views/company/project/create.blade.php
@@ -42,25 +42,40 @@
             <ul class="project-create__container__list">
                 <li class="project-create__container__list__item margin--none">
                     <div class="project-create__container__list__item__name">プロジェクト名 :</div>
-                    <div class="control">
-                        <input name="name" class="project-create__container__list__item__input under-border input" type="text" placeholder="プロジェクト名">
+                    <div class="input-container">
+                        <input name="project_name" class="project-create__container__list__item__input" type="text" value="{{ old('project_name')}}">
+                        @if($errors->has('project_name'))
+                            <div>
+                                <strong style='color: #e3342f;'>{{ $errors->first('project_name') }}</strong>
+                            </div>
+                        @endif
                     </div>
                 </li>
                 <li class="project-create__container__list__item">
                     <div class="project-create__container__list__item__name">プロジェクト詳細 :</div>
-                    <div class="control">
-                        <textarea class="project-create__container__list__item__textarea textarea has-fixed-size" name="detail" placeholder=""></textarea>
+                    <div class="textarea-container">
+                        <textarea class="project-create__container__list__item__textarea" name="project_detail" placeholder="">{{ old('project_detail') }}</textarea>
+                        @if($errors->has('project_detail'))
+                            <div>
+                                <strong style='color: #e3342f;'>{{ $errors->first('project_detail') }}</strong>
+                            </div>
+                        @endif
                     </div>
                 </li>
                 <li class="project-create__container__list__item">
                     <div class="project-create__container__list__item__name">担当者 :</div>
-                    <div class="select id-normal">
+                    <div class="select-container select id-normal">
                         <select name="company_user_id" class="select-box" id="company-staff-name-list">
                             <option></option>
-                            @foreach( $company_infos as $company_info )
-                            <option value={{ $company_info->id }}>{{ $company_info->name }}</option>
+                            @foreach( $company_users as $company_user )
+                            <option value={{ $company_user->id }}>{{ $company_user->name }}</option>
                             @endforeach
                         </select>
+                        @if($errors->has('company_user_id'))
+                            <div>
+                                <strong style='color: #e3342f;'>{{ $errors->first('company_user_id') }}</strong>
+                            </div>
+                        @endif
                     </div>
                 </li>
                 <li class="project-create__container__list__item">
@@ -68,10 +83,15 @@
                     <div class="select id-normal">
                         <select name="partner_id" class="select-box" id="partner-name-list">
                             <option value=""></option>
-                            @foreach( $partner_infos as $partner_info )
-                            <option value={{ $partner_info->id }}>{{ $partner_info->name }}</option>
+                            @foreach( $partner_users as $partner_user )
+                            <option value={{ $partner_user->id }}>{{ $partner_user->name }}</option>
                             @endforeach
                         </select>
+                        @if($errors->has('partner_id'))
+                            <div>
+                                <strong style='color: #e3342f;'>{{ $errors->first('partner_id') }}</strong>
+                            </div>
+                        @endif
                     </div>      
                 </li>
                 <li class="project-create__container__list__item">
@@ -79,36 +99,53 @@
                     <div class="calendars">
                         <div class="calendars__wrapper">
                             <div class="calendars__wrapper__title">開始日</div>
-                            <input name="started_at" type="text">
+                            <input name="started_at" type="text" value="{{ old('started_at')}}">
+                            @if($errors->has('started_at'))
+                                <div>
+                                    <strong style='color: #e3342f;'>{{ $errors->first('started_at') }}</strong>
+                                </div>
+                            @endif
                         </div>
-                        <div class="calendars__wrapper">
+                        <div class="calendars__wrapper right">
                             <div class="calendars__wrapper__title">終了日</div>
-                            <input name="ended_at" type="text">
+                            <input name="ended_at" type="text" value="{{ old('ended_at')}}">
+                            @if($errors->has('ended_at'))
+                                <div>
+                                    <strong style='color: #e3342f;'>{{ $errors->first('ended_at') }}</strong>
+                                </div>
+                            @endif
                         </div>
                     </div>
                 </li>
                 <li class="project-create__container__list__item">
                     <div class="project-create__container__list__item__name">予算 :</div>
-                    <div class="budget-container control">
-                        <input name="budget" class="input is-normal" type="text" placeholder=""><span class="budget-container__yen">円</span>
-                    </div>      
+                    <div class="budget-container input-container">
+                        <input name="budget" type="text" placeholder="" value="{{ old('budget')}}"><span class="budget-container__yen">円</span>
+                        @if($errors->has('budget'))
+                            <div>
+                                <strong style='color: #e3342f;'>{{ $errors->first('budget') }}</strong>
+                            </div>
+                        @endif
+                    </div>
                 </li>
                 <li class="project-create__container__list__item">
                     <div class="project-create__container__list__item__name">資料 :</div>
                     <div class="project-create__container__list__item__wrapper">
-                        <div class="project-create__container__list__item__wrapper__description">Upload</div>
-                        <div class="file is-boxed project-create__container__list__item__wrapper__filearea">
-                            <label class="file-label project-create__container__list__item__wrapper__filearea__label">
-                                <input class="file-input project-create__container__list__item__wrapper__filearea__label" type="file">
-                                <span class="file-cta project-create__container__list__item__wrapper__filearea__label__file-cta">
-                                <span class="file-icon">
-                                    <i class="fas fa-upload"></i>
-                                </span>
-                                <span class="file-label">
-                                    Choose a file…
-                                </span>
-                                </span>
-                            </label>
+                        <div class="project-create__container__list__item__wrapper__description">アップロード</div>
+                        <div class="file has-name is-boxed">
+                        <label class="file-label">
+                            <input id="inputFile" class="file-input" type="file" name="resume">
+                            <span id="upload-btn" class="file-cta">
+                            <span class="file-icon">
+                                <i class="fas fa-upload"></i>
+                            </span>
+                            <span class="file-label">
+                                Choose a file…
+                            </span>
+                            </span>
+                            <span id="fileName" class="file-name">
+                            </span>
+                        </label>
                         </div>
                     </div>
                 </li>
