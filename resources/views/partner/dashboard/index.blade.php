530a471999e60db243e597f8dff55ed291e19663
YumaUeno <y.ueno.1.1.1.5@gmail.com>
2019-07-12 14:50:22 +0900

    Fix link url

--- resources/views/partner/dashboard/index.blade.php
+++ resources/views/partner/dashboard/index.blade.php
@@ -28,8 +28,8 @@
             <ul class="menu-list menu menu__container__menu-list">
                 <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
                 <li><a href="/partner/dashboard" class="isActive"><i class="fas fa-chart-bar"></i>Dashboard</a></li>
-                <li><a href="/partner/project"><i class="fas fa-envelope"></i>プロジェクト</a></li>
-                <li><a href="/partner/task"><i class="fas fa-tasks"></i>タスク</a></li>
+                <li><a href="#"><i class="fas fa-envelope"></i>プロジェクト</a></li>
+                <li><a href="#"><i class="fas fa-tasks"></i>タスク</a></li>
                 <li><a href="/partner/invoice/create"><i class="fas fa-newspaper"></i>書類</a></li>
                 <li><a href="#"><i class="fas fa-calendar-alt"></i>Calendar</a></li>
                 <li><a href="#"><i class="fas fa-question"></i>Heip Center</a></li>
