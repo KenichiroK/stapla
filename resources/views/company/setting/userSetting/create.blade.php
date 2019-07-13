e3bdea5253aa1dd121a7cdd876f89832f3564cc7
YumaUeno <y.ueno.1.1.1.5@gmail.com>
2019-07-13 19:39:05 +0900

    Remove console_log

--- resources/views/company/setting/userSetting/create.blade.php
+++ resources/views/company/setting/userSetting/create.blade.php
@@ -9,7 +9,6 @@ const setPreview = (input) => {
   if (input.files && input.files[0]) {
     let reader = new FileReader();
     reader.onload = (e) => {
-      console.log(e)
       preview.src = e.target.result;
     }
     reader.readAsDataURL(input.files[0]);
--- resources/views/partner/profile/create.blade.php
+++ resources/views/partner/profile/create.blade.php
@@ -10,7 +10,6 @@ const setPreview = (input) => {
   if (input.files && input.files[0]) {
     let reader = new FileReader();
     reader.onload = (e) => {
-      console.log(e)
       preview.src = e.target.result;
     }
 
