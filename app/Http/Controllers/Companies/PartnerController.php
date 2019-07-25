8c6a5ada4424b93fa82057b6226614c8d6b1f173
YumaUeno <y.ueno.1.1.1.5@gmail.com>
2019-07-25 22:42:24 +0900

    Fix header profile in company partner

--- app/Http/Controllers/Companies/PartnerController.php
+++ app/Http/Controllers/Companies/PartnerController.php
@@ -15,9 +15,9 @@ class PartnerController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $company_id = CompanyUser::where('auth_id', $user->id)->get()->first()->company_id;
-        $partners = Partner::where('company_id', $company_id)->with(['projectPartners.project', 'TaskPartners.task'])->paginate(6);;
-        return view('company/partner/index', compact('partners'));
+        $company_user = CompanyUser::where('auth_id', $user->id)->get()->first();
+        $partners = Partner::where('company_id', $company_user->company_id)->with(['projectPartners.project', 'TaskPartners.task'])->paginate(6);;
+        return view('company/partner/index', compact('partners', 'company_user'));
     }
 
     public function store(Request $request)
@@ -61,9 +61,11 @@ class PartnerController extends Controller
 
     public function show($id)
     {
+        $user = Auth::user();
+        $company_user = CompanyUser::where('auth_id', $user->id)->get()->first();
         $tasks = TaskPartner::where('user_id', $id)->with(['task', 'task.project'])->get();
         $partners = Partner::with(['projectPartners.project', 'TaskPartners.task', ])->findOrFail($id);
-        return view('company/partner/show', compact('partners', 'tasks'));
+        return view('company/partner/show', compact('partners', 'tasks', 'company_user'));
     }
 
     public function edit($id)
