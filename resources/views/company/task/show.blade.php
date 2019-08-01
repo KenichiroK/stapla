3ce2270e216c826a096f3d54987691fc192af998
YumaUeno <y.ueno.1.1.1.5@gmail.com>
2019-08-01 17:31:15 +0900

    Fix condition shown button to change task status

--- resources/views/company/task/show.blade.php
+++ resources/views/company/task/show.blade.php
@@ -237,14 +237,14 @@
         </div>
         
         <div class="actionButton">
-            @if($task->status === 1 && $task->superior->id !== $company_user->id && $task->accounting->id !== $company_user->id)
+            @if($task->status === 1 && in_array($company_user->id, $company_user_ids))
                 <form action="{{ url('company/task/status') }}" method="POST">
                 @csrf
                     <input type="hidden" name="task_id" value="{{ $task->id }}">
                     <input type="hidden" name="status" value="2">
                     <button type="submit" class="done">上長に確認を依頼する</button>
                 </form>
-            @elseif($task->status === 2 && $task->superior->id === $company_user->id  && $task->accounting->id !== $company_user->id)
+            @elseif($task->status === 2 && $task->superior->id === $company_user->id)
                 <form action="{{ url('company/task/status') }}" method="POST">
                 @csrf
                     <input type="hidden" name="task_id" value="{{ $task->id }}">
@@ -257,39 +257,39 @@
                     <input type="hidden" name="status" value="3">
                     <button type="submit" class="done">タスクを承認する</button>
                 </form>
-            @elseif($task->status === 3 && $task->superior->id !== $company_user->id && $task->accounting->id !== $company_user->id)
+            @elseif($task->status === 3 && in_array($company_user->id, $company_user_ids))
                 <form action="{{ url('company/task/status') }}" method="POST">
                 @csrf
                     <input type="hidden" name="task_id" value="{{ $task->id }}">
                     <input type="hidden" name="status" value="4">
                     <button type="submit" class="done">パートナーに依頼する</button>
                 </form>
-            @elseif($task->status === 5 && $task->superior->id !== $company_user->id && $task->accounting->id !== $company_user->id)
+            @elseif($task->status === 5 && in_array($company_user->id, $company_user_ids))
                 <a href="/company/document/purchaseOrder/create/{{ $task->id }}" class="done">発注書を作成する</a>
-            @elseif($task->status === 6 && $task->superior->id !== $company_user->id && $task->accounting->id !== $company_user->id)
+            @elseif($task->status === 6 && in_array($company_user->id, $company_user_ids))
                 <form action="{{ url('company/task/status') }}" method="POST">
                 @csrf
                     <input type="hidden" name="task_id" value="{{ $task->id }}">
                     <input type="hidden" name="status" value="7">
                     <button type="submit" class="done">発注書の確認を上長に依頼する</button>
                 </form>
-            @elseif($task->status === 7 && $task->superior->id === $company_user->id  && $task->accounting->id !== $company_user->id)
+            @elseif($task->status === 7 && $task->superior->id === $company_user->id)
                 <a class="done" href="/company/document/purchaseOrder/{{ $purchaseOrder->id }}">発注書を確認する</a>
-            @elseif($task->status === 8 && $task->superior->id !== $company_user->id && $task->accounting->id !== $company_user->id)
+            @elseif($task->status === 8 && in_array($company_user->id, $company_user_ids))
                 <form action="{{ url('company/task/status') }}" method="POST">
                 @csrf
                     <input type="hidden" name="task_id" value="{{ $task->id }}">
                     <input type="hidden" name="status" value="9">
                     <button type="submit" class="done">発注書をパートナーに依頼する</button>
                 </form>
-            @elseif($task->status === 10 && $task->superior->id !== $company_user->id && $task->accounting->id !== $company_user->id)
+            @elseif($task->status === 10 && in_array($company_user->id, $company_user_ids))
                 <form action="{{ url('company/task/status') }}" method="POST">
                 @csrf
                     <input type="hidden" name="task_id" value="{{ $task->id }}">
                     <input type="hidden" name="status" value="11">
                     <button type="submit" class="done">請求書を依頼する</button>
                 </form>
-            @elseif($task->status === 12 && $task->superior->id !== $company_user->id && $task->accounting->id !== $company_user->id)
+            @elseif($task->status === 12 && in_array($company_user->id, $company_user_ids))
                 <a href="/company/document/invoice/{{ $invoice->id }}" class="done">請求書を確認する</a>
             @else
                 <p class="non-action-text">必要なアクションはありません</p>
