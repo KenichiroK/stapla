7c75c21c9ebc0cc74b4060e41cd8b8d3141e6a64
Kenichiro Komatsu <kenichiro.komatsu0517@gmail.com>
2019-09-04 16:23:45 +0900

    create_routing view_parojectShow

---
+++ resources/views/partner/project/show.blade.php
@@ -0,0 +1,141 @@
+@extends('partner.index')
+
+@section('assets')
+<link rel="stylesheet" href="{{ mix('css/company/common/index.css') }}">
+<link rel="stylesheet" href="{{ mix('css/partner/document/invoice/create.css') }}">
+@endsection
+
+@section('header-profile')
+<div class="header-proflie">
+    <div class="option">
+        <div class="user-name">
+            {{ $partner->name }}
+        </div>
+
+        <div class="icon-imgbox">
+            <img src="../../../images/icon_small-down.png" alt="">
+        </div>
+    </div>
+    
+    <div class="optionBox">
+        <div class="balloon">
+            <ul>
+                <li><a href="">プロフィール設定</a></li>
+                <li>
+                    <form method="POST" action="{{ route('partner.logout') }}">
+                        @csrf
+                        <button type="submit">ログアウト</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+        
+    </div>
+
+    <div class="user-imgbox">
+        <a href="/partner/profile">
+            <img src="/{{ str_replace('public/', 'storage/', $partner->picture) }}" alt="プロフィール画像">
+        </a>
+    </div>
+</div>
+@endsection
+
+@section('sidebar')
+<div class="sidebar__container">
+    <div class="sidebar__container__wrapper">
+        <aside class="menu menu__container">
+            <div class="menu__container--label">
+                <div class="menu-label">
+                    <img src="../../../images/logo.png" alt="logo">
+                </div>
+            </div>
+            <ul class="menu-list menu menu__container__menu-list">
+                <li>
+                    <a href="#">
+                        <div class="icon-imgbox">
+                            <img src="../../../images/icon_home.png" alt="">
+                        </div>
+                        <div class="textbox">
+                            ホーム
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="/partner/dashboard">
+                        <div class="icon-imgbox">
+                            <img src="../../../images/icon_dashboard.png" alt="">
+                        </div>
+                        <div class="textbox">
+                            ダッシュボード
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="icon-imgbox">
+                            <img src="../../../images/icon_inbox.png" alt="">
+                        </div>
+                        <div class="textbox">
+                            プロジェクト
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="icon-imgbox">
+                            <img src="../../../images/icon_products.png" alt="">
+                        </div>
+                        <div class="textbox">
+                            タスク
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="isActive">
+                        <div class="icon-imgbox">
+                            <img src="../../../images/icon_invoices.png" alt="">
+                        </div>
+                        <div class="textbox">
+                            書類
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="icon-imgbox">
+                            <img src="../../../images/icon_calendar.png" alt="">
+                        </div>
+                        <div class="textbox">
+                            カレンダー
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="icon-imgbox">
+                            <img src="../../../images/icon_help-center.png" alt="">
+                        </div>
+                        <div class="textbox">
+                            ヘルプセンター
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="/partner/setting/invoice">
+                        <div class="icon-imgbox">
+                            <img src="../../../images/icon_setting.png" alt="">
+                        </div>
+                        <div class="textbox">
+                            設定
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </aside>
+    </div>
+</div>
+@endsection
+
+@section('content')
+test
+@endsection
