3bb620c2a1a77dd071e366a9ef300a57b18e448d
Ken1roKomatsu <kenichiro.komatsu0517@gmail.com>
2019-09-16 16:43:34 +0900

    add_validation projectCreate

--- app/Http/Requests/Companies/CreateProjectRequest.php
+++ app/Http/Requests/Companies/CreateProjectRequest.php
@@ -24,30 +24,20 @@ class CreateProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            'project_name'     => 'required',
-            'project_detail'   => 'required',
+            'project_name'     => 'required | string | max:64',
+            'project_detail'   => 'required | string | max:200',
             'company_user_id'  => 'required',
             'superior_id'      => 'required',
             'accounting_id'    => 'required',
             'partner_id'       => 'required',
             'started_at'       => 'required',
             'ended_at'         => 'required | after:started_at',
-            'budget'           => 'required',
+            'budget'           => 'required | integer | digits_between:1,12',
         ];
     }
 
     public function messages()
     {
-        return [
-            'project_name.required'     => 'プロジェクト名を入力してください',
-            'project_detail.required'   => 'プロジェクト詳細を入力してください',
-            'company_user_id.required'  => '担当者を選択してください',
-            'superior_id.required'      => '上長を選択してください',
-            'accounting_id.required'    => '経理を選択してください',
-            'partner_id.required'       => 'パートナーを選択してください',
-            'started_at.required'       => '開始日を選択してください',
-            'ended_at.required'         => '終了日を正しく選択してください',
-            'budget.required'           => '予算を入力してください',
-        ];
+        // 
     }
 }
