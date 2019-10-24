fd248a2757226068d7c3b379681b62a47e3582af
YumaUeno <y.ueno.1.1.1.5@gmail.com>
2019-10-24 22:52:59 +0900

    Add document index of partner

---
+++ app/Http/Controllers/Partners/DocumentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Partners;
+
+use App\Models\Partner;
+use App\Models\Invoice;
+use App\Models\PurchaseOrder;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Auth;
+
+class DocumentController extends Controller
+{
+    public function index()
+    {
+        $partner = Auth::user();
+        $invoices = Invoice::where('partner_id', $partner->id)->get(); 
+        $purchaseOrders = PurchaseOrder::where('partner_id', $partner->id)->get();
+
+        return view('partner.document.index', compact('invoices', 'purchaseOrders', 'partner'));
+    }
+}
