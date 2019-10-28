bc4fdacc8b2e884b3ae741f5243aff1b0ddc9536
shingo <kurosawa1991@gmail.com>
2019-10-28 19:12:02 +0900

    minor fix

--- resources/views/company/auth/passwords/email.blade.php
+++ resources/views/company/auth/passwords/email.blade.php
@@ -18,7 +18,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
-                        <div class="card-header">{{ __('パスワードリセット') }}</div>
+                        <div class="card-header">パスワードリセット</div>
 
                         <div class="card-body">
                             @if (session('status'))
