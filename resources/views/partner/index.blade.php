d33a4fe693e0b45273563e8c3093454326f9f52b
YumaUeno <y.ueno.1.1.1.5@gmail.com>
2019-11-13 21:21:33 +0900

    Fix notification of partner

--- resources/views/partner/index.blade.php
+++ resources/views/partner/index.blade.php
@@ -7,6 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="auth-id" content="{{ Auth::user()->id }}">
+    <meta name="url" content="{{ env('APP_URL') }}">
 
     <title>Impro</title>
     <!-- favicon -->
