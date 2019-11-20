b7b3285f5540d3f413476882071605df209af4e8
shingo <kurosawa1991@gmail.com>
2019-11-20 20:26:46 +0900

    create done project view

--- app/Http/Controllers/Partners/ProjectController.php
+++ app/Http/Controllers/Partners/ProjectController.php
@@ -17,6 +17,11 @@ class ProjectController extends Controller
         $tasks = Task::where('partner_id', $partner->id)->get();
         $projectsAccordingTask;
         $projects = array();
+        // $projects = Project::where('projects.company_id', $partner->company_id)
+        //     ->where('projects.status', '!=', config('const.PROJECT_COMPLETE'))
+        //     ->join('tasks', 'projects.id', '=', 'tasks.project_id')
+        //     ->get();
+        
         if ($tasks->count() === 0) {
             return view('partner/project/index', compact('projects'));
         }
@@ -29,6 +34,18 @@ class ProjectController extends Controller
         }
 
         return view('partner/project/index', compact('projects'));
+    }
+    public function doneIndex()
+    {
+        $partner = Auth::user();
+        $projects = Project::where('company_id', $partner->company_id)->where('status', config('const.PROJECT_COMPLETE'))->get();
+
+        $task_count_arr = []; 
+        for($i = 0; $i < count($projects); $i++){
+            $taskCount = count($projects[$i]->tasks);
+            array_push($task_count_arr, $taskCount);
+        }
+        return view('partner/project/done-index', compact('projects', 'task_count_arr'));
     }
     public function show($project_id)
     {
