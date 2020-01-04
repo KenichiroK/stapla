3d4378efdd2022253cf44a9c5c396c942f9bbb66
yota-hada <hadayota33@gmail.com>
2020-01-04 16:27:49 +0900

    temp

--- resources/js/common/toggle-button.js
+++ resources/js/common/toggle-button.js
@@ -6,3 +6,13 @@ window.onload = function() {
     }
   };
 };
+
+$(function() {
+  $("input").keydown(function(e) {
+    if ((e.which && e.which === 13) || (e.keyCode && e.keyCode === 13)) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+});
