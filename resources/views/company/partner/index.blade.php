954e59d1a07bc831a2bc3c59a935bb2cb8bb49ce
yota-hada <hadayota33@gmail.com>
2020-01-12 23:49:03 +0900

    style: change page of 'views/company/partner/index.blade.php'.

--- resources/views/company/partner/index.blade.php
+++ resources/views/company/partner/index.blade.php
@@ -15,7 +15,7 @@
         @endif
 
         <div class="top-container">
-            <h1 class="top-container__title">パートナー</h1>
+            <h1 class="top-container__title">パートナ</h1>
             <div class="btn-a-container">
                 <a href="{{ route('company.invite.partner') }}">パートナー追加</a>
             </div>
@@ -24,7 +24,6 @@
         <div class="partner-content">
             <h3 class="partner-content__title">ステータス</h3>
 
-            {{-- TODO:　resources/views/partner/project/index.blade.phpのidがtab-buttonのやつをパクる --}}
             <ul class="partner-content__tab">
                 <li class="is-active">
                     <a class="tab-btn" href="">全て<span class="counter">(20)</span></a>
@@ -41,34 +40,67 @@
             </ul>
 
             <div class="partner-content__card-wrapper">
+
+                {{-- TODO: ダミーデータの要素は削除 --}}
                 <div class="card">
-                    <div class="card__content"></div>
-                    <div class="card__footer"></div>
-                </div>
-            </div>
-        </div>
-
-        <div class="temp" style="margin-bottom:100px;"></div>
-
-
-        <div class="profile-list">
-
-            @foreach( $partners as $partner )
-            <div class="profile-card-container">
-                <div class="profile-card-container__wrapper">
-                    <div class="main-content">
-                        <div class="main-content__img-container">
-                            <img src="{{ $partner->picture }}"  alt="">
+                    <div class="card__content">
+                        <div class="image-wrapper">
+                            <img class="profile-image" src="https://avatars2.githubusercontent.com/u/30946750?s=460&v=4" alt="logo">
                         </div>
-                        <div class="main-content__info-list">
-                            <div class="main-content__info-list__name">{{ $partner->name }}</div>
-                            <div class="main-content__info-list__job">{{ $partner->occupations }}</div>
-                            <div class="main-content__info-list__assessment-achievement"></div>
+                        <div class="name-wrapper">
+                            <p class="name">羽田 陽太</p>
+                            <p class="occupations">エンジニア</p>
                         </div>
                     </div>
+                    <div class="card__footer">
+                        <div class="circle complete-color"></div>
+                        <p class="status">契約締結済</p>
+                    </div>
                 </div>
+
+                <div class="card">
+                    <div class="card__content">
+                        <div class="image"></div>
+
+                    </div>
+                    <div class="card__footer">
+                        <div class="circle uncontracted-color"></div>
+                        <p class="status">未契約</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card__content">
+                        <div class="image"></div>
+
+                    </div>
+                    <div class="card__footer">
+                        <div class="circle progress-color"></div>
+                        <p class="status">契約作業中</p>
+                    </div>
+                </div>
+
+                @foreach( $partners as $partner )
+                <div class="card">
+                    <div class="card__content">
+                        <div class="image-wrapper">
+                            @if (isset($partner->picture))
+                            <img class="profile-image" src="{{ $partner->picture }}">
+                            @else
+                            <img class="profile-image" src="{{ env('AWS_URL').'/common/dummy_profile_icon.png' }}">
+                            @endif
+                        </div>
+                        <div class="name-wrapper">
+                            <p class="name">{{ $partner->name }}</p>
+                            <p class="occupations">{{ $partner->occupations }}</p>
+                        </div>
+                    </div>
+                    <div class="card__footer">
+                        <div class="circle complete-color"></div>
+                        <p class="status">契約締結済</p>
+                    </div>
+                </div>
+                @endforeach
             </div>
-            @endforeach
 
             <div class="pagenate-container">
                 <div class="pagenate-container__wrapper">
