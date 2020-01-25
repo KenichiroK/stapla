1ecba60409c6562bd56d9766c2e8a2135ea77e2a
yota-hada <hadayota33@gmail.com>
2020-01-25 14:50:29 +0900

    style: delete comment out in partner index page and resolve conflict.

--- resources/views/company/partner/index.blade.php
+++ resources/views/company/partner/index.blade.php
@@ -16,44 +16,30 @@
 
         <div class="top-container">
             <h1 class="top-container__title">パートナー</h1>
-                <!-- <p class="control has-icons-left serch-wrp">
-                    <input class="search-name input" type="text" placeholder="パートナーを検索">
-                    <span class="icon">
-                    <img src="{{ env('AWS_URL') }}/common/searchicon.png" alt="serch">
-                    </span>
-                </p> -->
-                <div class="btn-a-container">
-                    <a href="{{ route('company.invite.partner') }}">パートナー追加</a>
-                </div>
+            <div class="btn-a-container">
+                <a href="{{ route('company.invite.partner') }}">パートナー追加</a>
+            </div>
         </div>
-        
+
         <div class="profile-list">
-            @foreach($partners as $partner)
+
+            @foreach( $partners as $partner )
             <div class="profile-card-container">
                 <div class="profile-card-container__wrapper">
                     <div class="main-content">
                         <div class="main-content__img-container">
-                            <!-- <img class="main-content__img-container__img" src="" alt=""> -->
                             <img src="{{ $partner->picture }}"  alt="">
                         </div>
                         <div class="main-content__info-list">
                             <div class="main-content__info-list__name">{{ $partner->name }}</div>
                             <div class="main-content__info-list__job">{{ $partner->occupations }}</div>
-                            <div class="main-content__info-list__assessment-achievement">
-                                <!-- <div class="assessment">⭐⭐⭐⭐</div> -->
-                                <!-- <div class="achievement">実績<span class="num">1</span><span class="ken">件</span></div> -->
-                            </div>
+                            <div class="main-content__info-list__assessment-achievement"></div>
                         </div>
-                        <!-- <div class="main-content__edit-icons">
-                            <div>
-                                <img src="{{ env('AWS_URL') }}/common/edit.png" alt="">
-                            </div>
-                        </div> -->
                     </div>
                 </div>
             </div>
             @endforeach
-            
+
             <div class="pagenate-container">
                 <div class="pagenate-container__wrapper">
                     {{ $partners->links() }}
