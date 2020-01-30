0c6ecf609f403da7f82732c325befd4df523e1cc
Ken1roKomatsu <kenichiro.komatsu0517@gmail.com>
2020-01-30 17:08:55 +0900

    fix cannot editing task

--- app/Http/Requests/Companies/TaskPreviewRequest.php
+++ app/Http/Requests/Companies/TaskPreviewRequest.php
@@ -12,24 +12,35 @@ class TaskPreviewRequest extends FormRequest
     }
 
     public function rules()
-    {
-        return [
-            // タスク
-            'project_id'           => 'bail | required | uuid',
-            'task_name'            => 'bail | required | string | max:64',
-            'content'              => 'bail | required | string | max:200',
-            'task_company_user_id' => 'bail | required | uuid',
-            'superior_id'          => 'bail | required | uuid',
-            'accounting_id'        => 'bail | required | uuid',
-            'started_at'           => 'required',
-            'ended_at'             => 'bail | required | after:started_at | after:yesterday',
-            'partner_id'           => 'bail | required | uuid',
-            'order_price'          => 'bail | required | integer | digits_between:1, 12',
-            'delivery_date'        => 'required | after:started_at | after:yesterday',
-            // 発注書
-            'order_name'           => 'max:64',
-            'order_company_user'   => 'max:64',
-        ];
+    {  
+        if($this->task_status <= config('const.TASK_SUBMIT_SUPERIOR')){
+            return [
+                // タスク
+                'project_id'           => 'bail | required | uuid',
+                'task_name'            => 'bail | required | string | max:64',
+                'content'              => 'bail | required | string | max:200',
+                'task_company_user_id' => 'bail | required | uuid',
+                'superior_id'          => 'bail | required | uuid',
+                'accounting_id'        => 'bail | required | uuid',
+                'started_at'           => 'required',
+                'ended_at'             => 'bail | required | after:started_at | after:yesterday',
+                'partner_id'           => 'bail | required | uuid',
+                'order_price'          => 'bail | required | integer | digits_between:1, 12',
+                'delivery_date'        => 'required | after:started_at | after:yesterday',
+                // 発注書
+                'order_name'           => 'max:64',
+                'order_company_user'   => 'max:64',
+            ];
+        } else{
+            return [
+                'project_id'           => 'bail | required | uuid',
+                'task_company_user_id' => 'bail | required | uuid',
+                'superior_id'          => 'bail | required | uuid',
+                'accounting_id'        => 'bail | required | uuid',
+                'started_at'           => 'required',
+                'ended_at'             => 'bail | required | after:started_at | after:yesterday',
+            ];
+        }
     }
 
     public function attributes()
